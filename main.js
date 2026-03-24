7153b60f82ed5ff3452f8103a5e6b66a02c13a1b
ktynerdev <ktyner.dev@gmail.com>
2026-03-24 12:18:56 -0400

    Initialize Phaser game with GameScene

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+import GameScene from './scenes/GameScene.js';
 
+const config = {
+    type: Phaser.AUTO,
+    width: 400,
+    height: 600,
+    parent: 'game',
+    scene: [GameScene]
+};
+
+new Phaser.Game(config);
