6fae34fead093728bc168ed35ef6d45da1085d01
ktynerdev <ktyner.dev@gmail.com>
2026-03-24 13:31:36 -0400

    Adjust game dimensions and add scaling configuration
    Updated game configuration dimensions and added scaling options.

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ import GameScene from './scenes/GameScene.js';
 
 const config = {
     type: Phaser.AUTO,
-    width: 400,
-    height: 600,
+    width: 430,
+    height: 760,
     parent: 'game',
-    scene: [GameScene]
+    backgroundColor: '#0b1330',
+    scene: [GameScene],
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    }
 };
 
 new Phaser.Game(config);
